ca8ab73ad1f23a7ec9488c12d473fe54be8abe70
Joao Pedro Soares Brugnago <joao.psb20@gmail.com>
2024-03-10 17:53:39 -0300

    dependencias do txt

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ def serve_static(filename):
 
 if __name__ == '__main__':
     #app.run(debug=True)
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 5000))
     serve(app, host='0.0.0.0', port=port)
